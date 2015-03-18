8c0792047e9eae0d47ec931dd6d699a1410d41f5
Issam <issam.rachdi@gmail.com>
2015-03-18 20:02:05 +0000

    load buffer sound using XMLHttpRequest play sound and change volume

--- audiovisualizer.js
+++ audiovisualizer.js
@@ -2,16 +2,39 @@
 var context = new AudioContext();
 
 var audioBuffer;
+var gainNode;
 
-var request = new XMLHttpRequest();
+function loadSound() {
+  var request = new XMLHttpRequest();
 
-request.open('GET', 'Raksat_Atlas.mp3', true);
-request.responseType = 'arraybuffer';
+  request.open('GET', 'Raksat_Atlas.mp3', true);
+  request.responseType = 'arraybuffer';
 
-request.onload = function() {
-  context.decodeAudioData(request.response, function(decodedData) {
-    audioBuffer = decodedData;
-  });
-};
+  request.onload = function () {
+    context.decodeAudioData(request.response, function (decodedData) {
+      audioBuffer = decodedData;
+      console.log(audioBuffer.duration / 60);
+//      playSound(audioBuffer);
+    });
+  };
+  request.send();
+}
 
-request.send();
+function playSound(buffer) {
+  var source = context.createBufferSource();
+  gainNode = context.createGain();
+  source.buffer = buffer;
+  source.connect(gainNode);
+  gainNode.connect(context.destination);
+  source.start(0);
+  gainNode.gain.value = .5;
+}
+
+window.onload = function () {
+  loadSound();
+  document.getElementById('volume').value = 50;
+  document.getElementById('volume').addEventListener('change', function(event) {
+    var vol = event.target.value;
+    gainNode.gain.value = vol / 100;
+  })
+}
