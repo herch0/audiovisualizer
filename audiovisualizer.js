dab4429697b3a98f72f7f6a6e12b29ad5564754c
Issam <issam.rachdi@gmail.com>
2015-03-23 21:48:27 +0000

    presque fini

--- audiovisualizer.js
+++ audiovisualizer.js
@@ -2,7 +2,10 @@
 var context = new AudioContext();
 
 var audioBuffer;
-var gainNode;
+var source;
+var analyser;
+var div_visual;
+var barWidth;
 
 function loadSound() {
   var request = new XMLHttpRequest();
@@ -13,28 +16,53 @@ function loadSound() {
   request.onload = function () {
     context.decodeAudioData(request.response, function (decodedData) {
       audioBuffer = decodedData;
-      console.log(audioBuffer.duration / 60);
-//      playSound(audioBuffer);
+      analyser = context.createAnalyser();
+      analyser.fftSize = 256;
+      playSound(audioBuffer);
     });
   };
   request.send();
 }
 
 function playSound(buffer) {
-  var source = context.createBufferSource();
-  gainNode = context.createGain();
+  source = context.createBufferSource();
   source.buffer = buffer;
-  source.connect(gainNode);
-  gainNode.connect(context.destination);
+  source.connect(analyser);
+  analyser.connect(context.destination);
   source.start(0);
-  gainNode.gain.value = .5;
+  requestAnimationFrame(drawData);
+}
+
+function drawData() {
+  var dataLength = analyser.frequencyBinCount;
+  var arrayData = new Uint8Array(dataLength);
+  analyser.getByteTimeDomainData(arrayData);
+
+  barWidth = (div_visual.offsetWidth / dataLength);// * 2.5;
+  barWidth -= (barWidth/10);
+  console.log(div_visual.offsetWidth)
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < dataLength; i++) {
+    var bar = document.createElement('div');
+    bar.className = 'bar';
+    bar.style.width = barWidth + 'px';
+    bar.style.height = arrayData[i] + 'px';
+    fragment.appendChild(bar);
+  }
+  div_visual.innerHTML = '';
+  div_visual.appendChild(fragment);
+//  requestAnimationFrame(drawData);
+}
+
+function pause() {
+  source.stop();
+}
+
+function play() {
+  source.start();
 }
 
 window.onload = function () {
+  div_visual = document.getElementById("visualization");
   loadSound();
-  document.getElementById('volume').value = 50;
-  document.getElementById('volume').addEventListener('change', function(event) {
-    var vol = event.target.value;
-    gainNode.gain.value = vol / 100;
-  })
-}
+};
